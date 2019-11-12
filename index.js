1325abdf7e1b1a663d60b3b103753f55637b1256
nnk95 <nnk95@msn.com>
2019-11-12 15:41:17 +0900

    dirty hack for enabling DMs via useragent change

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = Franz => Franz;
+module.exports = Franz => class Instagram extends Franz {
+    overrideUserAgent() {const useragent = window.navigator.userAgent;
+
+        // Quick and dirty hackfix
+        const parts = useragent.split('Mozilla/5.0');
+    
+        return parts.join('Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1').replace('Electron', 'Safari').replace('Franz', 'Safari').replace('Chrome', 'Safari');
+    }
+};
